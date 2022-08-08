5abbe51658d716c5b6b838d6a54fa2b438907b42
95choiJH <80238745+95choiJH@users.noreply.github.com>
2022-08-08 15:22:14 +0900

    수정

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,7 +1,10 @@
 $(function(){
 
-
-
-
+    
+    $('button').click(function(){
+        var random = Math.floor(Math.random() * 99) + 1;
+        var question = $('li:nth-child('+random+') strong').text();
+        $('.q').text(question);
+    })
 
 })
